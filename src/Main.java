1153f7e13d76e3ad25592ca1f5880d386dc32902
Kate Pestrikova <volkva2@gmail.com>
2020-09-11 22:58:47 +0500

    added all files

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args)
+    {float ticket_cost;
+
+    {
+       ticket_cost = 10520F;
+       float miles_bonus = 0.05F;
+       float miles_count = ticket_cost * miles_bonus;
+        System.out.println(miles_count);
+    }
+    {
+       ticket_cost = 20200;
+       float miles_bonus = 0.05F;
+       float miles_count = ticket_cost * miles_bonus;
+       System.out.println(miles_count);
+    }
+
+}}
